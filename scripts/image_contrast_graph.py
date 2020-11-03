ef64dcce6f6721657f614b6f24e89567d4f8be79
joenguyen99 <67709929+joenguyen99@users.noreply.github.com>
2020-11-02 23:51:32 -0500

    User Clarification Updates
    11/2/2020
    ran autocropper
    ran input image
    ran image contrast graph
    all successfully

--- scripts/image_contrast_graph.py
+++ scripts/image_contrast_graph.py
@@ -7,17 +7,31 @@ Created on Fri Mar  6 10:26:16 2020
 """
 import numpy as np
 import skimage
+from skimage.color import rgb2gray
+from skimage.io import imread
 import matplotlib.pyplot as plt
+import os
 
+# allow for device input, comment out to use shortcut
+#directory = input("Enter directory path: ")
+#os.chdir(directory)
+#name = input("Enter image name: ")
 
-
+#shortcut - change "directory", change file "name"
+directory = r"C:\Users\joekh\Documents\GitHub\ML-Breat_Cancer_Classfier\images\contrast_images"
+os.chdir(directory)
 name = 'clear'
-image = skimage.color.rgb2gray(skimage.io.imread('/users/bencohen/onedrive/documents/rutgers/senior_design/images/' + name + '.jpeg'))
-y = image[400,:]
-x = np.arange(len(y))
 
-plt.plot(x, y)
-plt.xlabel('Pixel')
-plt.ylabel('Greyscale Value')
-plt.title('Clear')
-plt.ylim([0, 1])
+if(os.path.exists(name + '.jpeg')):
+    image = skimage.color.rgb2gray(skimage.io.imread(directory + "\\" + name + '.jpeg'))
+    y = image[400,:]
+    x = np.arange(len(y))
+
+    plt.plot(x, y)
+    plt.xlabel('Pixel')
+    plt.ylabel('Greyscale Value')
+    plt.title('Clear')
+    plt.ylim([0, 1])
+    plt.show()
+else:
+    print("File does not exist.")
