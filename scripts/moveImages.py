79cb8d1e259d8088b09d4b7a97bae2182d033d8c
kyle-t-lee <36808274+kyle-t-lee@users.noreply.github.com>
2020-12-08 14:30:36 -0500

    Update moveImages.py

--- scripts/moveImages.py
+++ scripts/moveImages.py
@@ -2,7 +2,8 @@ import random
 import os
 import shutil
 
-root= r"C:\Users\joekh\Desktop\Senior Design Code\BreaKHis_v1 - Copy copy\malignant\papillary_carcinoma"
+#root= r"C:\Users\joekh\Desktop\Senior Design Code\BreaKHis_v1 - Copy copy\malignant\papillary_carcinoma"
+root = r"C:\Users\Kyle Lee\Documents\GitHub\ML-Breat_Cancer_Classfier\images\BreaKHis_v1\BreaKHis_v1\histology_slides\breast\malignant\SOB\papillary_carcinoma"
 for SOB in os.listdir(root):
     SOBpath = os.path.join(root, SOB)
     if(os.path.isdir(SOBpath)):
